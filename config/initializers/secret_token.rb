420052e5abdf575482eb6a782d92c83f4a2a9f48
Taylor Yelverton <taylor@ideamarshal.com>
2012-04-05 18:08:00 -0500

    Init rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEStarLeague::Application.config.secret_token = '099b4df542ac7dc76d6cac3980bdb05fd0f6d561a22c73fd08a8a9261941fe0f2c0ee2e60664087c73aca36ae1381cda28afedb47b45124deea09b214275767b'
